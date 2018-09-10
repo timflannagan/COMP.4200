bc1df1fa048607c51276f59d7e8706f221fa9323
timflannagan1 <timflannagan@gmail.com>
2018-09-10 15:16:01 -0400

    Finish PS0a assignment

---
+++ ps0/shopSmart.py
@@ -0,0 +1,57 @@
+# shopSmart.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to
+# http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Here's the intended output of this script, once you fill it in:
+
+Welcome to shop1 fruit shop
+Welcome to shop2 fruit shop
+For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
+For orders:  [('apples', 3.0)] best shop is shop2
+"""
+
+import shop
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """
+    best_shop = None
+    lowest_total_cost = 0.0
+
+    for shop in fruitShops:
+        current_shop_price = shop.getPriceOfOrder(orderList)
+
+        if lowest_total_cost is 0.0:
+            best_shop = shop
+            lowest_total_cost = current_shop_price
+        elif lowest_total_cost > shop.getPriceOfOrder(orderList):
+            best_shop = shop
+            lowest_total_cost = current_shop_price
+
+    return best_shop
+
+if __name__ == '__main__':
+  "This code runs when you invoke the script from the command line"
+  orders = [('apples',1.0), ('oranges',3.0)]
+  dir1 = {'apples': 2.0, 'oranges':1.0}
+  shop1 =  shop.FruitShop('shop1',dir1)
+  dir2 = {'apples': 1.0, 'oranges': 5.0}
+  shop2 = shop.FruitShop('shop2',dir2)
+  shops = [shop1, shop2]
+  print "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+  orders = [('apples',3.0)]
+  print "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
---
+++ ps0/syllabus.py
@@ -0,0 +1,51 @@
+# syllabus.py
+# -----------
+# (C) 2018 J Mwaura, jonathan_mwaura@uml.edu
+# for use only at University of Massachusetts Lowell
+# DO NOT REDISTRIBUTE
+
+
+"""
+Run python autograder.py
+"""
+
+# instructions:
+# read the course syllabus to find the answers to the below questions.
+# then, modify the return values of each of the next three functions
+# to answer the question in each function.
+
+# note 1: the course syllabus is the course home page.
+
+# note 2: this question is not autograded locally. You must
+#   upload your solution to Bottlenose to see if you got it right.
+
+def publish_solutions():
+    "Are you allowed to share your solutions to class problem sets?"
+    "Return the Python object for true or false to correctly answer."
+
+    "*** YOUR CODE HERE ***"
+    return False
+
+def teams():
+    "What is the minimum and maximum size of teams for the final project?"
+    "Return a list of two items."
+    "The first item must be the minimum team size."
+    "The second item must be the maximum team size."
+
+    "*** YOUR CODE HERE ***"
+    return [2, 3]
+
+
+def maillist():
+    "Please join the course mailing list"
+    "so that you are apprised of critical and timely course info"
+    "Sign up at https://groups.google.com/forum/#!forum/comp4200artificial-intelligence"
+
+    "Once you are a member, find the message correct password"
+    "and copy it here."
+
+    "The message will have the subject line:"
+    "'PS0a syllabus.py maillist password'"
+
+    "*** YOUR CODE HERE ***"
+    return "password"
